8649995c3204d7047310a03863d8fb75a977dcc7
Nawaf <nawafalqari13@gmail.com>
2021-10-20 10:00:17 +0300

    v0.0.2

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def index():
         urlsData[code] = userURL
 
         with open('urls.json', 'w') as urlsFile:
-            dump(urlsData, urlsFile)
+            dump(urlsData, urlsFile, indent=3)
         
         return render_template('url.html', url=userURL, shortURL=code)
     else:
@@ -32,6 +32,6 @@ def index():
 def url(url):
     if url in urlsData:
         return redirect(urlsData[url])
-    return '<h1 style="font-family: sans-serif">404</h1><p>Page not found</p>'
+    return '<h1 style="font-family: sans-serif; font-size:40px">404</h1><p>Page not found</p>'
     
 app.run(port=5000)
